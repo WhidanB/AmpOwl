1918ef284084a480466594342e13a077bd54053e
WhidanB <danyphone95@gmail.com>
2023-05-16 16:55:28 +0200

    ajout police 	new file:   fonts.css 	new file:   fonts/Rainbow Season.otf 	new file:   fonts/rainbow_season-webfont.woff 	new file:   fonts/rainbow_season-webfont.woff2 	modified:   index.php 	modified:   style.css

--- index.php
+++ index.php
@@ -53,10 +53,14 @@ if ($_POST) {
 
 <body>
     <div class="modal hidden">
+        <h1>Ajouter une ampoule</h1>
         <form method="post">
-            <div>
-                <label for="date_amp">Date de changement</label>
-                <input type="date" name="date_amp" required>
+            <div class="form_container">
+                <div class="date">
+                    <label for="date_amp">Date de changement</label>
+
+                    <input type="date" name="date_amp" required>
+                </div>
                 <div class="select">
 
                     <label for="floor">Étage</label>
@@ -94,7 +98,10 @@ if ($_POST) {
     <div class="suppr hidden" id="supp">
 
         <h3>Voulez-vous vraiment supprimer cette ampoule ?</h3>
-        <a data-id="" class="confirmDel">Supprimer</a>
+        <div class="btn-sup-container">
+            <a data-id="" class="confirmDel">Supprimer</a>
+            <a href="">Annuler</a>
+        </div>
     </div>
 
     <header>
@@ -110,7 +117,11 @@ if ($_POST) {
         <h1>Mes ampoules</h1>
         <!-- <a href="add.php"> -->
         <div class="add">
-            <img src="plus-circle.svg" height="30px" width="30px" alt="Bouton Plus">
+            <svg width="30" height="30" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2a10 10 0 1 0 0 20 10 10 0 1 0 0-20z"></path>
+                <path d="M12 8v8"></path>
+                <path d="M8 12h8"></path>
+            </svg>
             <h3>Ajouter une ampoule</h3>
         </div>
         <!-- </a> -->
@@ -141,9 +152,15 @@ if ($_POST) {
                         <td><?= $ampoule['price'] . ' ' . "€" ?></td>
                         <td>
                             <a href="edit.php?id=<?= $ampoule['id'] ?>">
-                                <img src="edition.png" alt=""></a>
+                                <svg width="30" height="30" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                </svg></a>
                             <a data-id="<?= $ampoule['id'] ?>" class="cross">
-                                <img src="fermer.png" alt="">
+                                <svg width="30" height="30" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18 6 6 18"></path>
+                                    <path d="m6 6 12 12"></path>
+                                </svg>
                             </a>
                         </td>
                     </tr>
